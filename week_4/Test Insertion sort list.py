afacefc266b164fcdd9ee0f52fa3b83175e9b730
week_1 <55709746+vanikk06@users.noreply.github.com>
2019-10-15 17:37:38 +0800

    Create Test Insertion sort list.py

---
+++ week_4/Test Insertion sort list.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def insertionSortList(self, head: ListNode) -> ListNode:
+        if not head and not head.next:
+            return head
+        
+        out = head          #min
+        head = head.next
+        tail = out          #Max
+        
+        while head:
+            temp = head
+            head = head.next
+            if temp.val <= out.val:
+                temp.next = out
+                out = temp
+            elif temp.val >= tail.val:
+                tail.next = temp
+                tail = temp
+            else:
+                it = out
+                while it.next != tail or it.next.val < temp.val:
+                    it = it.next
+                temp.next = it.next
+                it.next = temp
+        tail.next = None
+        return out
